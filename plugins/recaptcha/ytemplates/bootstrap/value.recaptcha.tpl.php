3eb6dd83c0dfa9082c4f9466d0f206c3133c89ab
wolf359 <simonteufel@mac.com>
2023-01-09 16:46:44 +0100

    Update value.recaptcha.tpl.php
    Abruf des reCaptcha-Feldes in der aktuell gewählten Sprache.

--- plugins/recaptcha/ytemplates/bootstrap/value.recaptcha.tpl.php
+++ plugins/recaptcha/ytemplates/bootstrap/value.recaptcha.tpl.php
@@ -1,8 +1,9 @@
 <?php
 
-if (1 == $loadScript) {
-    echo '<script src="https://www.google.com/recaptcha/api.js" async defer></script>';
-}
+$clangcode = rex_clang::getCurrent()->getCode(); 
 
+if (1 == $loadScript) {
+    echo '<script src="https://www.google.com/recaptcha/api.js?hl='.$clangcode.'" async defer></script>';
+}
 ?>
 <div id="<?php echo $this->getHTMLId() ?>" class="g-recaptcha <?php echo $this->getWarningClass(); ?>" data-sitekey="<?php echo $publicKey ?>"></div>
